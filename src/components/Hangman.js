393e185de1044cb8d268caf9dbe89f2aafdfaaab
anjarupnik <anjarupnik@hotmail.com>
2017-11-04 22:14:57 +0100

    fixes for newgame

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -2,8 +2,6 @@ import React, { PureComponent } from 'react'
 import Title from './Title'
 import StartButton from './StartButton'
 import './Hangman.css'
-import AddLetter from './AddLetter'
-import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Man from '../images/man.jpg'
 import Man1 from '../images/man1.jpg'
@@ -16,7 +14,6 @@ class Hangman extends PureComponent {
     function refreshPage(){
     window.location.reload();
   }
-
     return (
       <div>
         <header>
@@ -24,14 +21,24 @@ class Hangman extends PureComponent {
         </header>
 
         <main>
-          { this.props.letter[1] > 1 && this.props.letter[1] > 6 ? <img className="man1" alt="hangman" src={Man1} /> : null }
-          { this.props.letter[1] > 2 && this.props.letter[1] > 6 ? <img className="man2" alt="hangman" src={Man2} /> : null }
-          { this.props.letter[1] > 3 && this.props.letter[1] > 6 ? <img className="man3" alt="hangman" src={Man3} /> : null }
-          { this.props.letter[1] > 4 && this.props.letter[1] > 6 ? <img className="man4 "alt="hangman" src={Man4} /> : null }
-          { this.props.letter[1] === 5 && this.props.letter[1] > 6 ? <img className="man" alt="hangman" src={Man} /> : null }
+          { this.props.letter[1] > 1  && this.props.letter[1] < 6 ?
+            <img className="man1" alt="hangman" src={Man1} /> : null }
+          { this.props.letter[1] > 2  && this.props.letter[1] < 6 ?
+            <img className="man2" alt="hangman" src={Man2} /> : null }
+          { this.props.letter[1] > 3  && this.props.letter[1] < 6 ?
+            <img className="man3" alt="hangman" src={Man3} /> : null }
+          { this.props.letter[1] > 4  && this.props.letter[1] < 6 ?
+            <img className="man4 "alt="hangman" src={Man4} /> : null }
+          { this.props.letter[1] === 5 && this.props.letter[1] < 6 ?
+            <img className="man" alt="hangman" src={Man} /> : null }
           <h1 className="word">{ this.props.letter[0] }</h1>
           <StartButton />
-          {this.props.letter[1] > 0 &&
+          {this.props.letter[0] === "You Won!!!!" &&
+              <button className="newgame" onClick={refreshPage}>
+                New Game
+              </button>
+            }
+          {this.props.letter[1] === 5 &&
               <button className="newgame" onClick={refreshPage}>
                 New Game
               </button>
